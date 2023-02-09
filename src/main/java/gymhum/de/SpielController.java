992e3b8f65480c73a873af0e9951c00a8b85e28e
vinzenzk <111504395+vinzenzk@users.noreply.github.com>
2023-02-09 13:09:49 +0100

    init 4

--- src/main/java/gymhum/de/SpielController.java
+++ src/main/java/gymhum/de/SpielController.java
@@ -34,6 +34,7 @@ public class SpielController {
         model.addAttribute("activePage", "Spiel");
         model.addAttribute("felder", getFelder());   
         model.addAttribute("gewonnen",  getG1());
+        model.addAttribute("Spieler", getS1());
         return "index.html";
     }
 
@@ -45,23 +46,33 @@ public class SpielController {
                 if(s1.getSpieler() == true) {
                     getFelder()[i][id].setFrei(false);
                     getFelder()[i][id].setXo(false);
-                    s1.setSpieler(false);
-                    System.out.println("Feld 77 wurde geändert");  
+                    s1.setSpieler(false); 
                     break;    
                 } 
                 else if(s1.getSpieler()== false) {
                     getFelder()[i][id].setFrei(false);
                     getFelder()[i][id].setXo(true);
-                    s1.setSpieler(true);   
-                    System.out.println("Feld wurde geändert");  
+                    s1.setSpieler(true);     
                     break;              
                 }             
             } 
         }
         testLauf();
-        System.out.println("HalloWler");
         return "redirect:/Spiel";
         
+    }
+
+    @GetMapping("/neuesSpiel") 
+    public String neuesSpiel(@RequestParam(name="activePage", required = true, defaultValue = "Spiel") String activePage) {
+        for(int i = 0; i <6; i++) {
+            for(int k = 0; k <7; k++){
+                getFelder()[i][k].setFrei(true);
+                getFelder()[i][k].setXo(false);
+            }
+        }
+        getG1().setGewonnen(false);
+
+        return "redirect:/Spiel";
     }
 
     private void initFeld() {
@@ -89,12 +100,10 @@ public class SpielController {
             for(int k = 0; k <=3; k++) {
                 if(getFelder()[i][k].getFrei()==false && getFelder()[i][k+1].getFrei()==false && getFelder()[i][k+2].getFrei()==false && getFelder()[i][k+3].getFrei()==false) {
                     if(getFelder()[i][k].getXo()==false && getFelder()[i][k+1].getXo()==false && getFelder()[i][k+2].getXo()==false && getFelder()[i][k+3].getXo()==false){
-                        System.out.println("2Spieler X hat gewonnen");
                         g1.setGewonnen(true);
                         
                     } else if(getFelder()[i][k].getXo() && getFelder()[i][k+1].getXo() && getFelder()[i][k+2].getXo() && getFelder()[i][k+3].getXo()) {
-                    System.out.println("2Spieler O hat gewonnen");
-                    g1.setGewonnen(true);
+                        g1.setGewonnen(true);
                     }
                 }
             }
@@ -104,11 +113,9 @@ public class SpielController {
             for(int k = 0; k <7; k++) {
                 if(getFelder()[i][k].getFrei()==false && getFelder()[i+1][k].getFrei()==false && getFelder()[i+2][k].getFrei()==false && getFelder()[i+3][k].getFrei()==false) {
                     if(getFelder()[i][k].getXo()==false && getFelder()[i+1][k].getXo()==false && getFelder()[i+2][k].getXo()==false && getFelder()[i+3][k].getXo()==false){
-                        System.out.println("3Spieler X hat gewonnen");
                         g1.setGewonnen(true);
                     } else if(getFelder()[i][k].getXo() && getFelder()[i+1][k].getXo() && getFelder()[i+2][k].getXo() && getFelder()[i+3][k].getXo()) {
-                    System.out.println("3Spieler O hat gewonnen");
-                    g1.setGewonnen(true);             
+                        g1.setGewonnen(true);             
                     }
                 }
             }
@@ -118,11 +125,9 @@ public class SpielController {
             for(int k = 0; k <=3 ; k++) {
                 if(getFelder()[i][k].getFrei()==false && getFelder()[i+1][k+1].getFrei()==false && getFelder()[i+2][k+2].getFrei()==false && getFelder()[i+3][k+3].getFrei()==false) {
                     if(getFelder()[i][k].getXo()==false && getFelder()[i+1][k+1].getXo()==false && getFelder()[i+2][k+2].getXo()==false && getFelder()[i+3][k+3].getXo()==false){
-                        System.out.println("4Spieler X hat gewonnen");
                         g1.setGewonnen(true);
                     } else if(getFelder()[i][k].getXo() && getFelder()[i+1][k+1].getXo() && getFelder()[i+2][k+2].getXo() && getFelder()[i+3][k+3].getXo()) {
-                    System.out.println("4Spieler O hat gewonnen");
-                    g1.setGewonnen(true);
+                        g1.setGewonnen(true);
                     }
                 }
             }
@@ -132,11 +137,9 @@ public class SpielController {
             for(int k = 0; k <=3 ; k++) {
                 if(getFelder()[i][k].getFrei()==false && getFelder()[i-1][k+1].getFrei()==false && getFelder()[i-2][k+2].getFrei()==false && getFelder()[i-3][k+3].getFrei()==false) {
                     if(getFelder()[i][k].getXo()==false && getFelder()[i-1][k+1].getXo()==false && getFelder()[i-2][k+2].getXo()==false && getFelder()[i-3][k+3].getXo()==false){
-                        System.out.println("5Spieler X hat gewonnen");
                         g1.setGewonnen(true);
                     } else if(getFelder()[i][k].getXo() && getFelder()[i-1][k+1].getXo() && getFelder()[i-2][k+2].getXo() && getFelder()[i-3][k+3].getXo()) {
-                    System.out.println("5Spieler O hat gewonnen");
-                    g1.setGewonnen(true);
+                        g1.setGewonnen(true);
                     }
                 }
             }
